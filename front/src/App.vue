d53f54a71ae615560b1fde8ae3db8cbfde3e3c42
Wiersinga <owiersinga@afi-sa.fr>
2024-02-18 21:50:47 +0100

    Create Footer component

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,32 @@
 
 <template>
-    <Header />
-    <CategoriesCarousel />
    
+    <header>
+        <Navbar2 />
+    <CategoriesCarousel />
+
+    </header>
+    <main>
+    <Product />
+    </main>
+
+    <footer>
+    <FooterApp />
+        
+    </footer>
 
 </template>
 
 <script setup>
-import Header from './components/Header.vue';
 import CategoriesCarousel from './components/CategroriesCarousel.vue';
 import Product from './components/Product.vue'
+import Navbar2 from './components/Navbar2.vue';
+import FooterApp from './components/FooterApp.vue';
 console.log('hi test');
 </script>
+
+<style>
+#products {
+    background-color: #2C666E;
+}
+</style>
---
+++ front/src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="btn">{{ name }}</button>
+</template>
+<script>
+export default {
+  props: {
+    name: String,
+  }
+}
+</script>
+<style>
+#btn {
+color: #B31414;
+}
+</style>
